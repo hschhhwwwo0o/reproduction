d02a57bb0811a656d400ea3a6b45c1fe9bc5d6a7
Saveliy <ssandry.github@gmail.com>
2021-12-11 14:44:04 +0300

    Mobile adaptive

--- components/Block/TheMainHeader/index.tsx
+++ components/Block/TheMainHeader/index.tsx
@@ -3,11 +3,11 @@ import Heading1 from "../../Text/Heading1";
 import { motion } from "framer-motion";
 
 const MainHeader: FunctionComponent = () => {
-  const spanStyle = (duration: number, y: number) => {
+  const spanStyle = (duration: number, y: number, rotate?: number) => {
     return {
       className: "block",
-      initial: { opacity: 0, y },
-      whileInView: { opacity: 1, y: 0 },
+      initial: { opacity: 0, y, rotate: rotate || 0 },
+      whileInView: { opacity: 1, y: 0, rotate: 0 },
       transition: { duration, bounce: 3 },
       viewport: { once: false },
     };
@@ -18,18 +18,18 @@ const MainHeader: FunctionComponent = () => {
       <div className="flex w-full h-screen items-center justify-center">
         <Heading1>
           <div className="flex">
-            <motion.span {...spanStyle(0.7, 30)}>R</motion.span>
-            <motion.span {...spanStyle(1.2, 250)}>E</motion.span>
+            <motion.span {...spanStyle(0.7, 30, -5)}>R</motion.span>
+            <motion.span {...spanStyle(1.2, 250, 12)}>E</motion.span>
             <motion.span {...spanStyle(2, 50)}>P</motion.span>
             <motion.span {...spanStyle(1, 190)}>R</motion.span>
             <motion.span {...spanStyle(2, 100)}>O</motion.span>
-            <motion.span {...spanStyle(2, 200)}>D</motion.span>
+            <motion.span {...spanStyle(2, 200, -15)}>D</motion.span>
             <motion.span {...spanStyle(1.4, 140)}>U</motion.span>
-            <motion.span {...spanStyle(0.9, 120)}>C</motion.span>
-            <motion.span {...spanStyle(2, 120)}>T</motion.span>
+            <motion.span {...spanStyle(0.9, 120, 20)}>C</motion.span>
+            <motion.span {...spanStyle(2, 120, 8)}>T</motion.span>
             <motion.span {...spanStyle(1.4, 20)}>I</motion.span>
             <motion.span {...spanStyle(0.2, 50)}>O</motion.span>
-            <motion.span {...spanStyle(1.6, 170)}>N</motion.span>
+            <motion.span {...spanStyle(1.6, 170, -12)}>N</motion.span>
           </div>
         </Heading1>
       </div>
--- components/Text/Heading1/index.tsx
+++ components/Text/Heading1/index.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from "react";
 
 const Heading1: FunctionComponent<IText> = ({ children, alignment = "left" }) => {
-  return <h1 className={`text-white text-8xl text-${alignment}`}>{children}</h1>;
+  return <h1 className={`text-white text-3xl lg:text-8xl text-${alignment}`}>{children}</h1>;
 };
 
 export default Heading1;
