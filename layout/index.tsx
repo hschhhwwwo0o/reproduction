43aea66bcb0c96e0a16218fd40b5cc999e9c7e62
Saveliy <ssandry.github@gmail.com>
2021-12-11 13:54:37 +0300

    Initialize framer motion

--- layout/index.tsx
+++ layout/index.tsx
@@ -1,10 +1,13 @@
 import React, { FunctionComponent } from "react";
+import { AnimatePresence } from "framer-motion";
 
 const ThaLayout: FunctionComponent = ({ children }) => {
   return (
-    <div className="flex items-center justify-center min-h-screen px-32">
-      <div className="w-full">{children}</div>
-    </div>
+    <AnimatePresence>
+      <div className="flex items-center justify-center min-h-screen px-32">
+        <div className="w-full">{children}</div>
+      </div>
+    </AnimatePresence>
   );
 };
 
