a08a81f789c0aecb93547b71643e05cd39642078
Saveliy <ssandry.github@gmail.com>
2021-12-11 12:48:40 +0300

    Empty

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <h1 className="text-white">hi</h1>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
