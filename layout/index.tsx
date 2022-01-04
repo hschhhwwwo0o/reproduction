ab7739e6c8a83b30acf034de18cb87f765a9ced0
Saveliy Andronov <ssandry.github@gmail.com>
2022-01-04 15:32:23 +0300

    TITLE

--- layout/index.tsx
+++ layout/index.tsx
@@ -1,9 +1,28 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
 import { AnimatePresence } from "framer-motion";
+import Head from "next/head";
 
 const ThaLayout: FunctionComponent = ({ children }) => {
+  const [title, setTitle] = useState<"REPRODUCTION" | "KILL">("REPRODUCTION");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (title === "REPRODUCTION") {
+        setTitle("KILL");
+      } else {
+        setTitle("REPRODUCTION");
+      }
+    }, 10);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [title]);
+
   return (
     <AnimatePresence>
+      <Head>
+        <title>{title}</title>
+      </Head>
       <div className="flex items-center justify-center min-h-screen px-4 lg:px-32">
         <div className="w-full">{children}</div>
       </div>
